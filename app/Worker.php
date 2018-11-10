860eb414dad328d60ad9bf440690f093fc56ea6a
Moh Awad <awad@snsoerp.com>
2018-11-10 10:30:34 +0200

    rename job_worker to worker_job !

--- app/Worker.php
+++ app/Worker.php
@@ -62,7 +62,7 @@ class Worker extends Model
     }
 
     public function jobs(){
-        return $this->belongsToMany(Job::class);
+        return $this->belongsToMany(Job::class, 'worker_job');
     }
 
 }
